f788e32bd5765f29c381cd6c77bd627eb58ddd55
Szymon Slowicki <szymon.slowicki@gmail.com>
2020-08-17 13:28:54 +0200

    Change name a file HelloWorld on firstlesson

---
+++ firstlesson.py
@@ -0,0 +1,11 @@
+
+
+print("TVP1", "TVP2", "TVN", "Polsat", "BBC", "HBO", "MTV", sep=(';'))
+print('\n')
+print("I like computers but better is TVP1 but better is TVP2" '\n' "but better is TVN but better is Polsat but better is BBC" '\n' "but better is HBO but better is MTV")
+print('\n')
+
+ProgramName = "BBC"
+Item= "News"
+Time= "18:00"
+print("I like watching ", Item, " at ", Time, " on ", ProgramName,".", sep=(''))
