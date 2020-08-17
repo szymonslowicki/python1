1be5e74e4c640ed955425cf3b835e7f11f67f506
Szymon Slowicki <szymon.slowicki@gmail.com>
2020-08-17 13:16:30 +0200

    Firtlesson with function print()

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,5 +5,12 @@ Spyder Editor
 This is a temporary script file.
 """
 
-print('Hello Wordl!')
-print(2+2)
+print("TVP1", "TVP2", "TVN", "Polsat", "BBC", "HBO", "MTV", sep=(';'))
+print('\n')
+print("I like computers but better is TVP1 but better is TVP2" '\n' "but better is TVN but better is Polsat but better is BBC" '\n' "but better is HBO but better is MTV")
+print('\n')
+
+ProgramName = "BBC"
+Item= "News"
+Time= "18:00"
+print("I like watching ", Item, " at ", Time, " on ", ProgramName,".", sep=(''))
